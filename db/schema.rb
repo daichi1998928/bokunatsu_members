667881fdcfa3fcb09b7a23661393746dc2bf4899
関口大地 <sekiguchidaichi@sekiguchidaichinoMacBook-Pro.local>
2020-06-06 18:58:00 +0900

    membersテーブルにpassword_digestカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,28 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_06_095357) do
+ActiveRecord::Schema.define(version: 2020_06_06_095256) do
 
-  create_table "assign_schools", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "assign_schools", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "branches_members", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "branches_members", force: :cascade do |t|
     t.integer "university_branch_id"
     t.integer "member_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "grades", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "grades", force: :cascade do |t|
     t.integer "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "links", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "links", force: :cascade do |t|
     t.integer "member_id"
     t.string "name"
     t.text "url"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 2020_05_06_095357) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "members", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "members", force: :cascade do |t|
     t.integer "title_of_branch_id"
     t.integer "university_id"
     t.integer "grade_id"
@@ -56,28 +56,29 @@ ActiveRecord::Schema.define(version: 2020_05_06_095357) do
     t.text "episode"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
-  create_table "personalities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "personalities", force: :cascade do |t|
     t.integer "member_id"
     t.string "content_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "title_of_branches", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "title_of_branches", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "universities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "universities", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "university_branches", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "university_branches", force: :cascade do |t|
     t.string "name"
     t.text "twitter_link"
     t.datetime "created_at", null: false

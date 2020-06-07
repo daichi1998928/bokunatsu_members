b6ca32697bc2a6b04dff598dfead78e5d66e9c24
関口大地 <sekiguchidaichi@sekiguchidaichinoMacBook-Pro.local>
2020-06-07 14:01:53 +0900

    基本的なメンバー情報追加機能を実装

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,12 +1,22 @@
 class Admin::MembersController < ApplicationController
   def new
     @new_member = Member.new
+    @grade = Grade.all
+    @title_of_branch = TitleOfBranch.all
+    @university = University.all
+    @assign_school = AssignSchool.all
   end
 
   def edit
   end
 
   def create
-    binding.pry
+    @new_member = Member.new(member_params)
+    @new_member.save!
   end
+
+  def member_params
+    params.require(:member).permit(:title_of_branch_id,:university_id,:grade_id,:assign_school_id,:name,:name_kana,:profile_image,:nickname,:faculty,:history,:deciding_factor,:objective,:episode,:password,:password_confimation)
+  end
+  
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '558cb0ae7a1e93ff3bc569f9d8b35a82fefe66b17071dcbe7c000ad4385887af19dd10d4ace2aa88818d04a1218171fee8d5d0d2d33d37281e039996c837ab9b'

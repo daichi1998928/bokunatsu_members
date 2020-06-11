882715ce04c95e20c41bb08cd1a0f3bdf694b9c4
関口大地 <sekiguchidaichi@sekiguchidaichinoMacBook-Pro.local>
2020-06-11 21:46:16 +0900

    交流小学校と支部名のseedファイルを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+CSV.foreach('db/seeds/assign_school.csv', headers: true) do |row|
+    AssignSchool.create!(
+      name: row
+    )
+end
+
+CSV.foreach('db/seeds/university_branch.csv', headers: true) do |f|
+    UniversityBranch.create!(
+      name: f["university_branch"],
+      twitter_link: f["Twitter_url"]
+    )
+end
+

bf0e78984777df74ed4c1db5297a27c22fe6ea75
関口大地 <sekiguchidaichi@sekiguchidaichinoMacBook-Pro.local>
2020-06-13 12:08:49 +0900

    personalitiesを追加できるように実装

--- app/models/member.rb
+++ app/models/member.rb
@@ -7,5 +7,8 @@ class Member < ApplicationRecord
   belongs_to :grade
   belongs_to :title_of_branch
   has_many :branches_members
+  has_many :personalities
+
+  accepts_nested_attributes_for :personalities, allow_destroy: true
   # branches_membersは運営部と支部メンバーを兼ねてるメンバーがいるのでhas_many
 end
--- app/models/personality.rb
+++ app/models/personality.rb
@@ -1,2 +1,3 @@
 class Personality < ApplicationRecord
+  belongs_to :member
 end
